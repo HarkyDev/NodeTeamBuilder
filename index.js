11b08c513c2ee44b60e8bcd3a02a71fe4d339c4f
Eoin Harkin <eharky@gmail.com>
2022-01-12 17:32:28 +0000

    can add manger object and then return it

--- index.js
+++ index.js
@@ -2,60 +2,116 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+const Employee = require("./lib/Employee")
+const Engineer = require("./lib/Engineer")
+const Manager = require("./lib/Manager.js")
+const Intern = require("./lib/Intern.js")
+const employeeArr = [];
 
-// TODO: Create an array of questions for user input
-const questions = [
-  {
-    type: "input",
-    name: "managerName",
-    message: "What is the Name of the manager?",
-  },
-  {
-    type: "input",
-    name: "managerEmployeeID",
-    message: "What is the mangers employee ID number?",
-  },
-  {
-    type: "input",
-    name: "managerEmail",
-    message: "What is the mangers email address?",
-  },
-  {
-    type: "input",
-    name: "managerOfficeNo",
-    message: "What is the mangers office Number?",
-  },
+function startQuestions() {
 
-  {
-    type: "list",
-    name: "addEmployee",
-    message: "what other employees would you like to add? ",
-    //most popular free choices according to opensource.org
-    choices: ["Intern" ,"Engineer", "I don't want to add anymore employees"],
+  const createManager = () => {
+    inquirer
+      .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the Name of the manager?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the mangers employee ID number?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the mangers email address?",
+    },
+    {
+      type: "input",
+      name: "officeNo",
+      message: "What is the mangers office Number?",
+    },
+    
+    {
+      type: "list",
+      name: "question",
+      message: "what other employees would you like to add? ",
+      choices: ["Intern" ,"Engineer", "I don't want to add anymore employees"],
   },
   
-
-];
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-//didn't do this as the functionality is created inside the init() function at 36-40
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions).then((answers) => {
-        const content = generateMarkdown(answers);
-        fs.writeFile("index.html", content, (err) =>
-          err
-            ? console.log(err)
-            : console.log("Created 'README.MD'")
+  
+  
+  
+])
+.then((data) => {
+        const managerData = new Manager(
+          data.name,
+          data.id,
+          data.email,
+          data.officeNo
         );
+        employeeArr.push(managerData);
+        console.log(employeeArr)
       });
+    };;
+  
+  createManager()
+
 }
 
-// Function call to initialize app
-init();
+    
 
 
-//team manager’s name, employee ID, email address, and office number#
-// engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-//intern’s name, ID, email, and school, and I am taken back to the menu
+
+
+startQuestions()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TODO: Create a function to initialize app
+// function managerFunction() {
+//     inquirer.prompt(managerQuestions).then((managerAnswers) => {
+//         const content = generateMarkdown(managerAnswers);
+//         fs.writeFile("index.html", content, (err) =>
+//           err
+//             ? console.log(err)
+//             : console.log("Created 'README.MD'")
+//         );
+//       });
+// }
+
